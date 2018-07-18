a1507587b0f4d15921e85dee2a496f13d5d94b1d
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-07-18 14:05:09 +0530

    remove logger-client

--- sequencer-ui-app/src/main/scala/tmt/sequencer/ui/r4s/Main.scala
+++ sequencer-ui-app/src/main/scala/tmt/sequencer/ui/r4s/Main.scala
@@ -5,22 +5,15 @@ import tmt.sequencer.ui.r4s.editor.EditorComponent
 import tmt.sequencer.ui.r4s.facade.NpmReactBridge
 import tmt.sequencer.ui.r4s.feeder.FeederComponent
 import tmt.sequencer.ui.r4s.logevent.LogEventComponent
-import tmt.sequencer.{SequenceEditorClient, SequenceFeederClient, SequenceLoggerClient, WebGateway}
+import tmt.sequencer._
 
 object Main {
   def main(arguments: Array[String]): Unit = {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-    val gateway                       = new WebGateway()
-    val clientF: SequenceFeederClient = new SequenceFeederClient(gateway)
-    val clientE: SequenceEditorClient = new SequenceEditorClient(gateway)
-    val clientL: SequenceLoggerClient = new SequenceLoggerClient()
-
     val component = E.div(
       Component(HeaderComponent),
-      Component(FeederComponent, clientF),
-      Component(EditorComponent, clientE),
-      Component(LogEventComponent, clientL)
+      Component(FeederComponent, SequencerClient.feeder),
+      Component(EditorComponent, SequencerClient.editor),
+      Component(LogEventComponent, SequencerClient.logger)
     )
     NpmReactBridge.renderToDomById(component, "main")
   }
--- sequencer-ui-app/src/main/scala/tmt/sequencer/ui/r4s/logevent/LogEventComponent.scala
+++ sequencer-ui-app/src/main/scala/tmt/sequencer/ui/r4s/logevent/LogEventComponent.scala
@@ -1,18 +1,18 @@
 package tmt.sequencer.ui.r4s.logevent
 
 import com.github.ahnfelt.react4s._
-import tmt.sequencer.SequenceLoggerClient
+import org.scalajs.dom.raw.EventSource
 import tmt.sequencer.ui.r4s.SequencerConstants
 import tmt.sequencer.ui.r4s.theme._
 
-case class LogEventComponent(client: P[SequenceLoggerClient]) extends Component[NoEmit] {
+case class LogEventComponent(client: P[EventSource]) extends Component[NoEmit] {
   val streamDataListS: State[List[String]] = State(List.empty[String])
 
   override def componentWillRender(get: Get): Unit = {
     if (get(streamDataListS).isEmpty) {
-      get(client).onLogEvent(x => {
+      get(client).onmessage = { x =>
         streamDataListS.set(get(streamDataListS) :+ s"$x\n")
-      })
+      }
     }
   }
 
@@ -27,6 +27,6 @@ case class LogEventComponent(client: P[SequenceLoggerClient]) extends Component[
   }
 
   override def componentWillUnmount(get: Get): Unit = {
-    get(client).closeEventSource()
+    get(client).close()
   }
 }
