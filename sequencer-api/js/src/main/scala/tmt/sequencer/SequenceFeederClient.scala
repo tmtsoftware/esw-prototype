bc6dbcd439dbbfe9e95699a4f3d18a8793f212b4
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-07-18 13:49:29 +0530

    Use web gateway in sequence feeder client

--- sequencer-api/js/src/main/scala/tmt/sequencer/SequenceFeederClient.scala
+++ sequencer-api/js/src/main/scala/tmt/sequencer/SequenceFeederClient.scala
@@ -1,26 +1,15 @@
 package tmt.sequencer
 
-import org.scalajs.dom.ext.Ajax
 import tmt.sequencer.api.SequenceFeederWeb
 import tmt.sequencer.models.{AggregateResponseWeb, CommandListWeb, WebRWSupport}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
-class SequenceFeederClient()(implicit ec: ExecutionContext) extends SequenceFeederWeb with WebRWSupport {
+class SequenceFeederClient(gateway: WebGateway) extends SequenceFeederWeb with WebRWSupport {
 
-  override def feed(commandList: CommandListWeb): Future[AggregateResponseWeb] = {
-    val url = s"${SequenceFeederWeb.ApiName}/${SequenceFeederWeb.Feed}"
-    Ajax
-      .post(
-        url = url,
-        data = upickle.default.write(commandList),
-        headers = Map(
-          "Content-Type" -> "application/json"
-        )
-      )
-      .map { xhr =>
-        println(xhr.responseText)
-        upickle.default.read[AggregateResponseWeb](xhr.responseText)
-      }
-  }
+  override def feed(commandList: CommandListWeb): Future[AggregateResponseWeb] = gateway.post(
+    url = s"${SequenceFeederWeb.ApiName}/${SequenceFeederWeb.Feed}",
+    data = upickle.default.write(commandList),
+    transform = x => upickle.default.read[AggregateResponseWeb](x)
+  )
 }
