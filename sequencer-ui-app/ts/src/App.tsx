b4e09638bd011ccb7e84822ba241379ca6e11fcd
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-07-23 11:46:16 +0530

    Refactor
    Co-authored-by: Bharat Singh <10626464+Bharat1103@users.noreply.github.com>

--- sequencer-ui-app/ts/src/App.tsx
+++ sequencer-ui-app/ts/src/App.tsx
@@ -6,7 +6,7 @@ import FeederComponent from './sequencer/components/feeder/FeederComponent';
 class App extends React.Component {
 
   public render() {
-    const client = new Client();
+    const client = new Client('http://localhost:8000');
     return (
       <div className="App">
         <FeederComponent client={client}/>
--- sequencer-ui-app/ts/src/sequencer/client/IFeederClient.tsx
+++ sequencer-ui-app/ts/src/sequencer/client/IFeederClient.tsx
@@ -1,16 +1,22 @@
 import * as request from "superagent";
 
 class Client {
+    private baseUrl: string;
+
+    constructor(baseUrl: string) {
+        this.baseUrl = baseUrl
+    }
+
     public feedApi = (input: string, callback: (res: string) => void) => {
         request
-            .post('http://localhost:8000/feeder/feed')
+            .post(`${this.baseUrl}/feeder/feed`)
             .set('Content-Type', 'application/json')
             .send(input)
             .then(res => callback(JSON.stringify(res.body, null, 2)), err => {
                     if (err.response) {
                         callback(err.response.text)
                     } else {
-                        callback("Oops something went wrong !!, please verify input.")
+                        callback("Oops something went wrong !!")
                     }
                 }
             );
