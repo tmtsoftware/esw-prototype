2d29d3293fbf023f3e69f061bacb15deeb13f0e9
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-08-13 11:49:01 +0530

    Remove Path object - inlining hashpath within main component
    Co-authored-by: Mushtaq Ahmed <5066+mushtaq@users.noreply.github.com>

--- sequencer-ui-app/src/main/scala/tmt/sequencer/MainComponent.scala
+++ sequencer-ui-app/src/main/scala/tmt/sequencer/MainComponent.scala
@@ -2,14 +2,15 @@ package tmt.sequencer
 
 import com.github.ahnfelt.react4s._
 import org.scalajs.dom
-import tmt.Path
 import tmt.sequencer.r4s.HeaderComponent
 
 case class MainComponent() extends Component[NoEmit] {
-  val page = State(Routes.router.data(Path.hashPath))
+  private def hashPath: String = dom.window.location.hash.drop(1)
+
+  val page = State(Routes.router.data(hashPath))
 
   dom.window.onhashchange = { _ =>
-    page.set(Routes.router.data(Path.hashPath))
+    page.set(Routes.router.data(hashPath))
   }
 
   override def render(get: Get): Node = {
--- sequencer-web-api/js/src/main/scala/tmt/Path.scala
+++
@@ -1,7 +0,0 @@
-package tmt
-
-import org.scalajs.dom
-
-object Path {
-  def hashPath: String = dom.window.location.hash.drop(1)
-}
