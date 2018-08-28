7d5f564e4ff44cb99855042a06365ce4fd2f3eca
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-08-28 15:05:38 +0530

    cleanup

--- sequencer-framework/src/main/scala/tmt/sequencer/RemoteRepl.scala
+++ sequencer-framework/src/main/scala/tmt/sequencer/RemoteRepl.scala
@@ -2,15 +2,10 @@ package tmt.sequencer
 
 import akka.actor.typed.ActorRef
 import ammonite.sshd._
-import csw.messages.commands.{CommandName, Observe, Setup, Wait}
-import csw.messages.params.models.{Id, ObsId, Prefix}
 import org.apache.sshd.server.auth.password.AcceptAllPasswordAuthenticator
 import tmt.sequencer.api.{SequenceEditor, SequenceFeeder}
 import tmt.sequencer.dsl.CswServices
-import tmt.sequencer.messages.SequencerMsg.{Pause, Resume}
 import tmt.sequencer.messages.SupervisorMsg
-import tmt.sequencer.messages.SupervisorMsg.ControlCommand
-import tmt.sequencer.models.CommandList
 
 // find a workaround for server not hanging after multiple connects/disconnects
 // explore timeout thing
@@ -28,9 +23,14 @@ class RemoteRepl(commandService: CswServices,
       passwordAuthenticator = Some(AcceptAllPasswordAuthenticator.INSTANCE) // or publicKeyAuthenticator
     ),
     predef = """
-         |def setFlags() = repl.compiler.settings.Ydelambdafy.value = "inline"
          |import scala.concurrent.duration.Duration
          |import scala.concurrent.{Await, Future}
+         |import csw.messages.params.generics.KeyType._
+         |import csw.messages.params.models._
+         |import csw.messages.commands._
+         |import tmt.sequencer.messages.SequencerMsg._
+         |import tmt.sequencer.messages.SupervisorMsg._
+         |import tmt.sequencer.models.CommandList
          |implicit class RichFuture[T](val f: Future[T]) {
          |  def get: T = Await.result(f, Duration.Inf)
          |}
@@ -40,18 +40,7 @@ class RemoteRepl(commandService: CswServices,
       "sequencer"      -> sequencer,
       "sequenceFeeder" -> sequenceFeeder,
       "sequenceEditor" -> sequenceEditor,
-      "Setup"          -> Setup,
-      "Observe"        -> Observe,
-      "Wait"           -> Wait,
-      "CommandList"    -> CommandList,
-      "CommandName"    -> CommandName,
-      "Prefix"         -> Prefix,
-      "ObsId"          -> ObsId,
-      "supervisor"     -> supervisor,
-      "Id"             -> Id,
-      "ControlCommand" -> ControlCommand,
-      "Pause"          -> Pause,
-      "Resume"         -> Resume
+      "supervisor"     -> supervisor
     )
   )
 }
