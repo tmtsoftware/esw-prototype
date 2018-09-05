66866d30ad98a73826fb0f9b436b180e85ff4738
weiss <weiss.personal@gmail.com>
2018-09-04 17:32:22 -0700

    handleSetupCommand and handleObserveCommand added, so command doesn't need to be casted.

--- sequencer-framework/src/main/scala/tmt/sequencer/dsl/CommandDsl.scala
+++ sequencer-framework/src/main/scala/tmt/sequencer/dsl/CommandDsl.scala
@@ -1,6 +1,6 @@
 package tmt.sequencer.dsl
 
-import csw.messages.commands.SequenceCommand
+import csw.messages.commands.{Observe, SequenceCommand, Setup}
 import org.tmt.macros.StrandEc
 import tmt.sequencer.Sequencer
 import tmt.sequencer.models.AggregateResponse
@@ -11,7 +11,15 @@ abstract class CommandDsl(sequencer: Sequencer) extends ScriptDsl {
   val commandHandlerBuilder: FunctionBuilder[SequenceCommand, Future[AggregateResponse]] = new FunctionBuilder
 
   def handleCommand(name: String)(handler: SequenceCommand => Future[AggregateResponse]): Unit = {
-    commandHandlerBuilder.addHandler(_.commandName.name == name)(handler)
+    commandHandlerBuilder.addHandler[SequenceCommand](_.commandName.name == name)(handler)
+  }
+
+  def handleSetupCommand(name: String)(handler: Setup => Future[AggregateResponse]): Unit = {
+    commandHandlerBuilder.addHandler[Setup](_.commandName.name == name)(handler)
+  }
+
+  def handleObserveCommand(name: String)(handler: Observe => Future[AggregateResponse]): Unit = {
+    commandHandlerBuilder.addHandler[Observe](_.commandName.name == name)(handler)
   }
 
   def nextIf(f: SequenceCommand => Boolean)(implicit strandEc: StrandEc): Future[Option[SequenceCommand]] =
