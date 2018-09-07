d5aba3a2b502040809891d42dd3b415d0345ced1
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-09-07 12:20:38 +0530

    Incorporate example use cases for loop construct in script

--- sequencer-scripts-test/src/main/scala/scripts/iris/IrisDarkNight.scala
+++ sequencer-scripts-test/src/main/scala/scripts/iris/IrisDarkNight.scala
@@ -4,6 +4,42 @@ import tmt.sequencer.ScriptImports._
 
 class IrisDarkNight(cs: CswServices) extends Script(cs) {
 
+  val publisherStream = cs.publish(10.seconds) {
+    SystemEvent(Prefix("iris-test"), EventName("system"))
+  }
+
+  val subscriptionStream = cs.subscribe(Set(EventKey("iris-test.system"))) { _ =>
+    println(s"------------------> received-event for ocs on key-------------------->")
+    Done
+  }
+
+  var flag = true
+  def setupAssemblyLoop = loop {
+    spawn {
+      println("************ loop **************")
+      val command = Setup(Prefix("test"), CommandName("test-command"), None)
+      cs.setup("Sample1Assembly", command).await
+      stopWhen(flag)
+    }
+  }
+
+  cs.handleCommand("setup-start-loop") { command =>
+    spawn {
+      println(s"[Iris] Received command: ${command.commandName}")
+      flag = false
+      setupAssemblyLoop
+      AggregateResponse.markSuccessful(command)
+    }
+  }
+
+  cs.handleCommand("setup-stop-loop") { command =>
+    spawn {
+      println(s"[Iris] Received command: ${command.commandName}")
+      flag = true
+      AggregateResponse.markSuccessful(command)
+    }
+  }
+
   cs.handleCommand("setup-iris") { command =>
     spawn {
       println(s"[Iris] Received command: ${command.commandName}")
