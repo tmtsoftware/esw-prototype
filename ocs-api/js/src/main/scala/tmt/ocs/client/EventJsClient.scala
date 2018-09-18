8b2d809acb013870000f8cbedf9266f8851232c9
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-09-18 15:29:21 +0530

    Fix EventJsClient

--- ocs-api/js/src/main/scala/tmt/ocs/client/EventJsClient.scala
+++ ocs-api/js/src/main/scala/tmt/ocs/client/EventJsClient.scala
@@ -6,11 +6,11 @@ import tmt.ocs.{EventStream, WebGateway}
 
 class EventJsClient(gateway: WebGateway) extends AssemblyJsonSupport {
   def subscribeToEvents(subsystem: String, component: Option[String] = None, event: Option[String] = None): EventStream[Event] = {
-    val componentAttr = component.map(c => s"component=$c")
-    val eventAttr     = event.map(e => s"event=$e")
-    val attrs         = componentAttr ++ eventAttr.mkString("&")
-    val attrStr       = if (attrs.isEmpty) "" else s"?$attrs"
-    val url           = s"/event/subscribe/$subsystem$attrStr"
+    val componentAttr: Option[String] = component.map(c => s"component=$c")
+    val eventAttr: Option[String]     = event.map(e => s"event=$e")
+    val attrs: String                 = (componentAttr ++ eventAttr).mkString("&")
+    val attrStr                       = if (attrs.isEmpty) "" else s"?$attrs"
+    val url                           = s"/event/subscribe/$subsystem$attrStr"
     gateway.stream[Event](url)
   }
 }
