68c72cfeeb65a61f4b9695dcf083c78c2f01bf10
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-09-19 22:02:47 +0530

    Minor fix for commandDsl

--- ocs-framework/src/main/scala/tmt/ocs/util/ScriptLoader.scala
+++ ocs-framework/src/main/scala/tmt/ocs/util/ScriptLoader.scala
@@ -8,6 +8,6 @@ object ScriptLoader {
   //should load take params and remove a few dependencies
   def load(configs: Configs, cswServices: CswServices): Script = {
     val clazz = getClass.getClassLoader.loadClass(configs.scriptClass)
-    clazz.getConstructor(classOf[CswServices], classOf[CommandDsl]).newInstance(cswServices).asInstanceOf[Script]
+    clazz.getConstructor(classOf[CswServices]).newInstance(cswServices).asInstanceOf[Script]
   }
 }
