a8875483585f2a30af4f17014d9bd2c93c7d841d
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-10-11 18:13:43 +0530

    Add predef for embedded ammonite

--- ocs-framework/src/main/scala/ocs/framework/TestAmmoniteApp.scala
+++ ocs-framework/src/main/scala/ocs/framework/TestAmmoniteApp.scala
@@ -10,9 +10,19 @@ object TestAmmoniteApp {
     LoggingSystemFactory.start("sample", "", "", system)
 
     ammonite
-      .Main(
-        predefCode = "println(\"Start with location service, command service, sequencer APIs!\")"
-      )
+      .Main(predefCode = """
+           |import scala.concurrent.duration.Duration
+           |import scala.concurrent.{Await, Future}
+           |import csw.params.core.generics.KeyType._
+           |import csw.params.commands._
+           |import csw.params.core.models._
+           |import ocs.api.messages.SequencerMsg._
+           |import ocs.api.messages.SupervisorMsg._
+           |import ocs.api.models.CommandList
+           |implicit class RichFuture[T](val f: Future[T]) {
+           |  def get: T = Await.result(f, Duration.Inf)
+           |}
+           """.stripMargin)
       .run(
         "locationService" -> locationServiceWrapper,
         "commandService"  -> commandServiceWrapper,
