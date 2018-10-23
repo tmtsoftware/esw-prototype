a1d52d1ae9356215849e1075c80a3ffad1301cad
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-10-23 09:44:20 +0530

    Update csw latest sha
    Co-authored-by: Mushtaq Ahmed <5066+mushtaq@users.noreply.github.com>

--- project/Libs.scala
+++ project/Libs.scala
@@ -27,7 +27,7 @@ object Enumeratum {
 
 object Csw {
   private val Org     = "com.github.tmtsoftware.csw"
-  private val Version = "fed98e33d" //change this to 0.1-SNAPSHOT to test with local csw changes (after publishLocal)
+  private val Version = "3925a61c4" //change this to 0.1-SNAPSHOT to test with local csw changes (after publishLocal)
 
   val `csw-location-client` = Org %% "csw-location-client" % Version
   val `csw-command-client`  = Org %% "csw-command-client"  % Version
