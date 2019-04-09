f8f9c1f67e131663e0850ae9d562668527d2974c
Poorva Gokhale <poorvasgokhale@gmail.com>
2019-04-09 11:26:37 +0530

    #000: Poorva: Fix serialisation for Either type. Add kryo configurations

--- ocs-client/src/main/scala/ocs/client/Main.scala
+++ ocs-client/src/main/scala/ocs/client/Main.scala
@@ -24,7 +24,7 @@ object Main {
                 |import csw.params.core.models._
                 |import ocs.api.messages.SequencerMsg._
                 |import ocs.api.messages.SupervisorMsg._
-                |import ocs.api.messages.ScriptLoaderMsg._
+                |import ocs.api.messages.ScriptCommand._
                 |import csw.location.api.models.ComponentId
                 |import ocs.api.models.Sequence
                 |implicit class RichFuture[T](val f: Future[T]) {
