c9d9aa6a523a15d35d10d37233f3562a59f3f845
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-05-01 08:26:50 +0530

    ignore experimental code

--- laminar-spikes/src/main/scala/todo/components/Link.scala
+++ laminar-spikes/src/main/scala/todo/components/Link.scala
@@ -1,21 +1,21 @@
-package todo.components
-import com.raquo.laminar.api.L._
-import todo.context.{TodoListContext, VisibilityFilterContext}
-import todo.models.VisibilityFilter
-
-class Link private (
-    val node: Div
-)
-
-object Link {
-  def apply(linkText: VisibilityFilter) = {
-    val sink: Var[Unit] = Var(())
-    val node = button(
-      linkText,
-      onClick.map {
-        onSubmit.preventDefault.map(_ => TodoListContext.add(text1.now())) --> sink.writer,
-       ( _ => VisibilityFilterContext.visibilityFilter.set(linkText)) --> sink.writer
-      }
-    )
-  }
-}
+//package todo.components
+//import com.raquo.laminar.api.L._
+//import todo.context.{TodoListContext, VisibilityFilterContext}
+//import todo.models.VisibilityFilter
+//
+//class Link private (
+//    val node: Div
+//)
+//
+//object Link {
+//  def apply(linkText: VisibilityFilter) = {
+//    val sink: Var[Unit] = Var(())
+//    val node = button(
+//      linkText,
+//      onClick.map {
+//        onSubmit.preventDefault.map(_ => TodoListContext.add(text1.now())) --> sink.writer,
+//       ( _ => VisibilityFilterContext.visibilityFilter.set(linkText)) --> sink.writer
+//      }
+//    )
+//  }
+//}
