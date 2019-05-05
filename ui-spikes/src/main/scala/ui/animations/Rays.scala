bf03868dac27baa183903720476130ee6d84ae2f
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-05-05 09:26:55 +0530

    Better clicks

--- ui-spikes/src/main/scala/ui/animations/Rays.scala
+++ ui-spikes/src/main/scala/ui/animations/Rays.scala
@@ -2,7 +2,7 @@ package ui.animations
 
 import org.scalajs.dom.ext.Castable
 import typings.stdLib.stdLibStrings.Event
-import typings.svgDotJsLib.svgDotJsMod.{Line, Path, Shape, ^ => SVG}
+import typings.svgDotJsLib.svgDotJsMod.{Element, Line, Path, Shape, ^ => SVG}
 
 class Rays {
   JsMorph
@@ -14,34 +14,37 @@ class Rays {
   val light: Path        = SVG.get("light").cast[Path]
   val intialPath: String = light.array().toString
 
+  val shutter1: Element    = SVG.get("shutter-1")
   val topShutter1: Path    = SVG.get("top-shutter-1").cast[Path]
   val bottomShutter1: Path = SVG.get("bottom-shutter-1").cast[Path]
 
+  val shutter2: Element    = SVG.get("shutter-2")
   val topShutter2: Path    = SVG.get("top-shutter-2").cast[Path]
   val bottomShutter2: Path = SVG.get("bottom-shutter-2").cast[Path]
 
-  def animate(topShutter: Path, bottomShutter: Path): Unit = {}
+  val irms: Element = SVG.get("irms")
+  val iris: Element = SVG.get("iris")
+  val wfs: Element  = SVG.get("wfs")
 
   val gates = List(
-    new Gate(topShutter1, -25, bottomShutter1),
-    new Gate(bottomShutter1, 25, topShutter1),
-    new Gate(mirror1, 75),
-    new Gate(mirror2, 75),
-    new Gate(topShutter2, -25, bottomShutter2),
-    new Gate(bottomShutter2, 25, topShutter2)
+    new Gate(topShutter1, -25)(shutter1),
+    new Gate(bottomShutter1, 25)(shutter1),
+    new Gate(mirror1, 75)(),
+    new Gate(mirror2, 75)(),
+    new Gate(topShutter2, -25)(shutter2),
+    new Gate(bottomShutter2, 25)(shutter2)
   )
 
   def animate(): Unit = {
     gates.foreach(_.animate())
+    new Destination(irms).animate()
   }
 
   def blockingShape(): Shape = {
     gates.find(_.open == false).map(_.shape).getOrElse(mirror3)
   }
 
-  class Gate(val shape: Shape, delta: Double, related: Shape*) {
-    val light: Path = SVG.get("light").cast[Path]
-
+  class Gate(val shape: Shape, delta: Double)(related: Element = shape) {
     var nextY: Double = shape.y() + delta
     var open          = false
 
@@ -51,13 +54,27 @@ class Rays {
       nextY = currentY
       open = !open
 
-      val targetX = blockingShape().rbox().cx - 5
-      light.animate().cast[Path].plot(intialPath + s"H $targetX")
+      val targetX = blockingShape().rbox().cx - 15
+      light.animate(500).cast[Path].plot(intialPath + s"H $targetX")
     }
 
     def animate(): Unit = {
-      shape.click(onClick _)
-      related.foreach(_.click(onClick _))
+      related.click(onClick _)
+    }
+  }
+
+  class Destination(elm: Element) {
+    var selected = false
+
+    def onClick(e: Event): Unit = {
+      selected = true
+      val x = elm.rbox().x
+      val y = elm.rbox().y
+      light.animate(500).cast[Path].plot(light.array().toString + s" L $x $y")
+    }
+
+    def animate(): Unit = {
+      elm.click(onClick _)
     }
   }
 }
